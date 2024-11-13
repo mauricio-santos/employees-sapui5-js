83e9a37aca1c7ed8cf8be6d9590dce23fb5cd213
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-11-13 12:37:15 +0000

    Implementando primeiras lógica ao controle

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,12 +1,36 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller) {
-    "use strict";
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel 
+     */
+    function (Controller, JSONModel) {
+        "use strict";
 
-    return Controller.extend("logaligroup.employees.controller.MainView", {
-        onInit: function () {
+        const Main = Controller.extend("logaligroup.employees.controller.MainView", {});
 
+        Main.prototype.onInit = function() {
+            
         }
+
+        Main.prototype.onInputLiveChange = function () {
+            const input = this.getView().byId("idEmployeeInput");
+            const value = input.getValue();
+            const maxLength = input.getMaxLength();
+            const valueLength = value.length;
+            const label = this.getView().byId("idCountryLabel");
+            const select = this.getView().byId("idSelect");
+
+            if (valueLength === maxLength) {
+                label.setVisible(true);
+                select.setVisible(true);
+            } else {
+                label.setVisible(false);
+                select.setVisible(false);
+            }
+        };
+
+        return Main;
     });
-});
