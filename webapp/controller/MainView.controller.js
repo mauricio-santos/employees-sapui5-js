4e63d1fca9116cc0600a3b241a5246230650b1b7
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-11-13 12:31:27 +0000

    Implementando lógica no controller

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -7,6 +7,23 @@ function (Controller) {
     return Controller.extend("logaligroup.employees.controller.MainView", {
         onInit: function () {
 
+        },
+
+        onInputLiveChange: function () {
+            const input = this.getView().byId("idEmployeeInput");
+            const value = input.getValue();
+            const maxLength = input.getMaxLength();
+            const valueLength = value.length;
+            const label = this.getView().byId("idCountryLabel");
+            const select = this.getView().byId("idSelect");
+
+            if (valueLength === maxLength) {
+                label.setVisible(true);
+                select.setVisible(true);
+            } else {
+                label.setVisible(false);
+                select.setVisible(false);
+            }
         }
     });
 });
