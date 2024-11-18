359402b93960cd5665ca31815eee1eb84729deda
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-11-18 07:26:11 +0000

    Implementação dos filtros

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,12 +1,16 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.ui.model.json.JSONModel} JSONModel 
+     * @param {typeof sap.ui.model.Filter} Filter 
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator 
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, Filter, FilterOperator) {
         "use strict";
 
         function onInit() {
@@ -21,28 +25,42 @@ sap.ui.define([
             // })
 
             oView.setModel(oJSONModel);
-        }
-
-        let Main = Controller.extend("logaligroup.employees.controller.MainView", {});
-
-        Main.prototype.onInit = onInit;
-
-        Main.prototype.onEmployeeIdInputLiveChange = function () {
-            const input = this.getView().byId("idEmployeeIdInput");
-            const value = input.getValue();
-            const maxLength = input.getMaxLength();
-            const valueLength = value.length;
-            const label = this.getView().byId("idCountryLabel");
-            const select = this.getView().byId("idListCountrySelect");
-
-            if (valueLength === maxLength) {
-                label.setVisible(true);
-                select.setVisible(true);
-            } else {
-                label.setVisible(false);
-                select.setVisible(false);
-            }
         };
+
+        function onFilterButtonPress() {
+            const oJSON = this.getView().getModel().getData();
+            const countryKey = oJSON.CountryKey;
+            const employeeId = oJSON.EmployeeId;
+            const filters = [];
+
+            if (countryKey){
+                filters.push(new Filter("Country", FilterOperator.EQ, countryKey))
+            }
+            
+            if (employeeId){
+                filters.push(new Filter("EmployeeID", FilterOperator.EQ, employeeId))
+            }
+            
+            const oList = this.getView().byId("idEmployeesTable");
+            const oBindig = oList.getBinding("items");
+            oBindig.filter(filters)
+        };
+
+        function onClearFilterButtonPress() {
+            const oModel = this.getView().getModel();
+            oModel.setProperty("/EmployeeId", "");
+            oModel.setProperty("/CountryKey", "");
+
+            const oList = this.getView().byId("idEmployeesTable");
+            const oBindig = oList.getBinding("items");
+            oBindig.filter([])
+            
+        };
+
+        const Main = Controller.extend("logaligroup.employees.controller.MainView", {});
+        Main.prototype.onInit = onInit;
+        Main.prototype.onFilterButtonPress = onFilterButtonPress;
+        Main.prototype.onClearFilterButtonPress = onClearFilterButtonPress;
 
         return Main;
     });
