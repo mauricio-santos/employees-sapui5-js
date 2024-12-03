6a1716439d17918772638da28770246a9aff107d
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-12-03 07:26:33 +0000

    Utilizando propiedade groupHeaderFactory no agrupamento de ítens

--- webapp/controller/ListTypes.controller.js
+++ webapp/controller/ListTypes.controller.js
@@ -1,15 +1,17 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/m/GroupHeaderListItem"
 ], 
 
 /**
  * 
  * @param {typeof sap.ui.core.mvc.Controller} Controller 
- * @param { typeof sap.ui.model.json.JSONModel} JSONModel 
+ * @param { typeof sap.ui.model.json.JSONModel} JSONModel
+ * @param { typeof sap.m.GroupHeaderListItem} GroupHeaderListItem
  * 
  */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, GroupHeaderListItem) {
         "use strict";
 
         return Controller.extend("logaligroup.employees.controller.ListTypes", {    
@@ -19,6 +21,18 @@ sap.ui.define([
                 const mockdataSource = "./localService/mockdata/ListData.json";
                 oJSONModel.loadData(mockdataSource);
                 this.getView().setModel(oJSONModel);
+            },
+
+            getGroupHeader: function (oGroup){
+                console.log(oGroup);
+                
+                const groupHeaderListItem = new GroupHeaderListItem({
+                    title: oGroup.key,
+                    upperCase: true,
+                    tooltip: oGroup.key
+                });
+                return groupHeaderListItem;
+
             }
 
 
