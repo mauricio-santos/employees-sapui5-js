d7cc823a29cc1186d8bd6391a118f3857a869d46
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-12-03 13:18:18 +0000

    Removendo ítens e refatoração do contador de seleção

--- webapp/controller/ListTypes.controller.js
+++ webapp/controller/ListTypes.controller.js
@@ -36,31 +36,66 @@ sap.ui.define([
             },
 
             onShowSelectedItemsButtonPress: function() {
-                let standardList = this.getView().byId("idProductsStdList");
-                let selectItems = standardList.getSelectedItems();
+                const standardList = this.getView().byId("idProductsStdList");
+                const selectedItems = standardList.getSelectedItems();
                 const i18nModel = this.getView().getModel("i18n").getResourceBundle();
+                const sMsgSelected = i18nModel.getText("selected");
 
-                if (selectItems.length > 0){
+                if (selectedItems.length > 0){
                     let text = "";
-                    selectItems.forEach( (item, index) => {
+                    selectedItems.forEach( (item, index) => {
                         const context = item.getBindingContext();
                         const oContext = context.getObject();
                         text += `${index+1}. ${oContext.Material}\n`;
-
-                        MessageToast.show(text.toUpperCase());
-                    })                              
+                    });
+                    MessageToast.show(sMsgSelected + ":\n" + text.toUpperCase());                           
                 }else{
-                    MessageToast.show(i18nModel.getText("noSelection"))
+                    MessageToast.show(i18nModel.getText("noSelection"));
                 }
             },
 
             onProductsListSelectionChange: function() {
-                let btnSelectedItems = this.getView().byId("idShowSelectedItemsButton");
-                let selectedItems = this.getView().byId("idProductsStdList").getSelectedItems().length;
+                const btnSelectedItems = this.getView().byId("idShowSelectedItemsButton");
+                const selectedItemsCount = this.getView().byId("idProductsStdList").getSelectedItems().length;
                 const oBundle = this.getView().getModel("i18n").getResourceBundle();
-                const sMsg = oBundle.getText("btnSelectedItems", [selectedItems])
-                
+                const sMsg = oBundle.getText("btnSelectedItems", [selectedItemsCount]);
+
                 btnSelectedItems.setProperty("text", sMsg);
+            },
+            
+            onDeleteSelectedItemsButtonPress: function() {
+                const standardList = this.getView().byId("idProductsStdList");
+                const selectedItems = standardList.getSelectedItems();
+                const btnSelectedItems = this.getView().byId("idShowSelectedItemsButton");
+                const i18nModel = this.getView().getModel("i18n").getResourceBundle();
+                const sMsgRemoved = i18nModel.getText("removed");
+                const sMsgNoSelection = i18nModel.getText("noSelection");
+                const oModel = this.getView().getModel();
+                const products = oModel.getProperty("/Products");
+                const arrId = [];
+               
+                if (selectedItems.length > 0) {
+                    let text = "";
+                    selectedItems.forEach((item, index) => {
+                        const context = item.getBindingContext();
+                        const oContext = context.getObject();
+                        
+                        arrId.push(oContext.Id);                     
+                        text += `${index + 1}. ${oContext.Material}\n`;
+                    });
+
+                    const filteredProducts = products.filter((prod) => {
+                        return !arrId.includes(prod.Id);
+                    })
+
+                    oModel.setProperty("/Products", filteredProducts);
+                    standardList.removeSelections();                   
+                    btnSelectedItems.setProperty("text", sMsgNoSelection);
+                    MessageToast.show(sMsgRemoved + ":\n" + text.toUpperCase());  
+                }else{
+                    MessageToast.show(sMsgNoSelection);
+                }
             }
+            
         });
 });
