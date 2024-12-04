0ddb395fc54603f2d369fc5c23ff50bb5215b517
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-12-04 13:49:13 +0000

    Criando tabelas dinâmicas para os pedidos

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -2,17 +2,15 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator",
-    "sap/m/MessageToast"
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.ui.model.json.JSONModel} JSONModel 
      * @param {typeof sap.ui.model.Filter} Filter 
      * @param {typeof sap.ui.model.FilterOperator} FilterOperator 
-     * @param {typeof sap.m.MessageToast} MessageToast 
      */
-    function (Controller, JSONModel, Filter, FilterOperator, MessageToast) {
+    function (Controller, JSONModel, Filter, FilterOperator) {
         "use strict";
 
         function onInit() {
@@ -84,24 +82,94 @@ sap.ui.define([
             model.setProperty("/visibleCity", false);
             model.setProperty("/visibleBtnShowCity", true);
             model.setProperty("/visibleBtnHideCity", false);
-        }
+        };
 
-        function onColumnListItemPress(event) {            
-            const itemPress = event.getSource(); //Obtendo item pressionado
-            const oContext = itemPress.getBindingContext("employeesModel"); //Obtendo o contexto
-            const oItem = oContext.getObject(); //Obtendo o objeto
-            const postalCode = oItem.PostalCode; //Obtendo o código postal
+        function showOrders(event) {
+            const itemPressed = event.getSource();
+            const oContext = itemPressed.getBindingContext("employeesModel");
+            const oEmployee = oContext.getObject();
+            const orders = oEmployee.Orders;
+            
+            const ordersList = [];
+            orders.forEach(order => {
+                ordersList.push(new sap.m.ColumnListItem({
+                    cells: [
+                        new sap.m.Label({ text: order.OrderID}),
+                        new sap.m.Label({ text: order.Freight }),
+                        new sap.m.Label({ text: order.ShipAddress })
+                    ]
+                }));
+            });
 
-            MessageToast.show(postalCode); 
-        }
+            const newTable = new sap.m.Table({
+                columns: [
+                    new sap.m.Column({header: new sap.m.Label({ text: "{i18n>orderID}" })}),
+                    new sap.m.Column({header: new sap.m.Label({ text: "{i18n>freight}" })}),
+                    new sap.m.Column({header: new sap.m.Label({ text: "{i18n>shipAddress}" })})
+                ],
+                items: ordersList,
+                width: "auto"
+            }).addStyleClass("sapUiSmallMargin");
+
+            const ordersTable = this.getView().byId("idOrdersHBox");
+            ordersTable.destroyItems(); //Previde chamdas duplicadas
+            ordersTable.addItem(newTable);
+
+            //Realizando o mesmo processo com outra lógica
+            const newTableJSON = new sap.m.Table();
+            newTable.setWidth("auto");
+            newTableJSON.addStyleClass("sapUiSmallMargin");
+
+            const columnOrderID = new sap.m.Column();
+            const labelOrderID = new sap.m.Label();
+            labelOrderID.bindProperty("text", "i18n>orderID");
+            columnOrderID.setHeader(labelOrderID);
+            newTableJSON.addColumn(columnOrderID);
+
+            const columnFreight = new sap.m.Column();
+            const labelFreight = new sap.m.Label();
+            labelFreight.bindProperty("text", "i18n>freight");
+            columnFreight.setHeader(labelFreight);
+            newTableJSON.addColumn(columnFreight);
+
+            const columnShipAddress = new sap.m.Column();
+            const labelShipAddress = new sap.m.Label();
+            labelShipAddress.bindProperty("text", "i18n>shipAddress");
+            columnShipAddress.setHeader(labelShipAddress);
+            newTableJSON.addColumn(columnShipAddress);
+            
+            const columListItems = new sap.m.ColumnListItem();
+
+            const cellLabelOrderID = new sap.m.Label();
+            cellLabelOrderID.bindProperty("text", "employeesModel>OrderID");
+            columListItems.addCell(cellLabelOrderID);
+
+            const cellLabelFreight = new sap.m.Label();
+            cellLabelFreight.bindProperty("text", "employeesModel>Freight");
+            columListItems.addCell(cellLabelFreight);
+
+            const cellLabelShipAddress = new sap.m.Label();
+            cellLabelShipAddress.bindProperty("text", "employeesModel>ShipAddress");
+            columListItems.addCell(cellLabelShipAddress);
+
+            const oBindigInfo = {
+                model: "employeesModel",
+                path: "Orders",
+                template: columListItems
+            };
+
+            newTableJSON.bindAggregation("items", oBindigInfo);
+            newTableJSON.bindElement("employeesModel>" + oContext.getPath());
+            ordersTable.addItem(newTableJSON);
+        };     
 
         const Main = Controller.extend("logaligroup.employees.controller.MainView", {});
         Main.prototype.onInit = onInit;
         Main.prototype.onFilterButtonPress = onFilterButtonPress;
         Main.prototype.onClearFilterButtonPress = onClearFilterButtonPress;
-        Main.prototype.onColumnListItemPress = onColumnListItemPress;
         Main.prototype.onShowCityButtonPress = onShowCityButtonPress;
         Main.prototype.onHideCityButtonPress = onHideCityButtonPress;
+        Main.prototype.showOrders = showOrders;
 
         return Main;
     });
