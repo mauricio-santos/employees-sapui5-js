0a42ad803163106234042862c1818ec6f4652028
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-12-20 11:13:11 +0000

    Eliminando incidências e corrigindo fragmentos duplicados na inserção

--- webapp/controller/EmployeeDetails.controller.js
+++ webapp/controller/EmployeeDetails.controller.js
@@ -18,29 +18,67 @@ sap.ui.define([
             const tableIncidence = this.getView().byId("idTableIncidencePanel");
             const incidenceModel = this.getView().getModel("incidenceModel");
             const oData = incidenceModel.getData();
-            const lastIndex = oData.length;
-            
-            oData.push({ index: lastIndex + 1 });
+            let oDataLength = oData.length;
+            let fragRandomId = 'fragID' + Date.now() + '-' + Math.random().toString(36).substring(2, 9);
+
+            //Adicionando index da incidencia no oData
+            oData.push({
+                index: oDataLength + 1,
+            });
+
+            //Atualizando o modelo
             incidenceModel.refresh();
-            
-            this._newIncidence = Fragment.load({
+
+            //Instanciando Fragmento
+            Fragment.load({
                 name: "logaligroup.employees.fragments.NewIncidence",
-                id: "fragIncidenceId" + lastIndex,
+                id: fragRandomId,
                 controller: this //Se o controller não for passado, a função dataFormat não executará
-            })
-            this._newIncidence.then(function (frag) {
+            }).then(function (frag) {
                 frag.bindElement({
                     model: "incidenceModel",
-                    path: "incidenceModel>/" + lastIndex
+                    path: "incidenceModel>/" + oDataLength
                 });
                 tableIncidence.addContent(frag);
-            })            
+            })
         };
+
+        function onIconDeletePress(event) {                     
+            const incidenceModel = this.getView().getModel("incidenceModel");
+            let oData = incidenceModel.getData();
+            let fragmentInstance = event.getSource().getParent().getParent(); // O avô do evento
+            const oBindingContext = fragmentInstance.getBindingContext("incidenceModel");
+            const tableIncidence = this.getView().byId("idTableIncidencePanel");
+            const contextObj = oBindingContext.getObject()
+
+            //Removendo elemento do modelo
+            oData.splice(contextObj.index - 1, 1);
+        
+            // Reordenando os índices do modelo
+            oData.forEach((oIncidence, i) => {
+                oIncidence.index = i + 1;          
+            });
+
+            //Atualizando o modelo
+            incidenceModel.refresh();
+
+            //Removendo o conteúdo da view
+            tableIncidence.removeContent(fragmentInstance);
+
+            // Destruindo o fragmento para liberar memória
+            fragmentInstance.destroy();
+           
+            // Realizando o binding na tabela
+            tableIncidence.getContent().forEach((row, index) => {
+                row.bindElement("incidenceModel>/" + index)
+            });  
+        }
 
         const EmployeeDetails = Controller.extend("logaligroup.employees.controller.EmployeeDetails", {});
         EmployeeDetails.prototype.onInit = onInit;
         EmployeeDetails.prototype.onCreateIncidenceButtonPress = onCreateIncidenceButtonPress;
         EmployeeDetails.prototype.Formatter = formatter;
+        EmployeeDetails.prototype.onIconDeletePress = onIconDeletePress;
         return EmployeeDetails;
     }
 );
