8c2e518951c5249ad9e0ba61a63e646cb5049e54
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2025-01-03 12:25:34 +0000

    Histórico de navegação

--- webapp/controller/OrderDetails.controller.js
+++ webapp/controller/OrderDetails.controller.js
@@ -1,8 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/ui/core/UIComponent"
+], 
+/**
+ * 
+ * @param {typeof sap.ui.core.mvc.Controller} Controller 
+ * @param {typeof sap.ui.core.routing.History} History 
+ * @param {typeof sap.ui.core.UIComponent} UIComponent 
+ * @returns 
+ */
+    function (Controller, History, UIComponent) {
     "use strict";
 
     return Controller.extend("logaligroup.employees.controller.OrderDetails", {
+
+        onButtonBackPress: function(event) {
+            const oHistory = History.getInstance();
+            const sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined){
+                window.history.go(-1);
+            }else {
+                const oRouter = UIComponent.getRouterFor(this);
+                oRouter.navTo("RouteMainView");
+            }
+        }
     });
 });
